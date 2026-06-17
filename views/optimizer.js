f122f7f5f702932201d1ab8d6d2e1cef55407ae5
James W <150986379+AutoTechWebDev@users.noreply.github.com>
2026-06-17 18:26:04 -0400

    Create optimizer.js

---
+++ views/optimizer.js
@@ -0,0 +1,15 @@
+export function renderOptimizer(content){
+
+    content.innerHTML = `
+
+        <div class="panel">
+
+            <h2>Optimizer</h2>
+
+            <p>
+                Coming in Module 5
+            </p>
+
+        </div>
+    `;
+}
