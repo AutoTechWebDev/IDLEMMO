b77d92a97732c1935161a6a497dc0b1722c79da1
James W <150986379+AutoTechWebDev@users.noreply.github.com>
2026-06-17 18:25:31 -0400

    Create builds.js

---
+++ views/builds.js
@@ -0,0 +1,15 @@
+export function renderBuilds(content){
+
+    content.innerHTML = `
+
+        <div class="panel">
+
+            <h2>Build Planner</h2>
+
+            <p>
+                Coming in Module 4
+            </p>
+
+        </div>
+    `;
+}
