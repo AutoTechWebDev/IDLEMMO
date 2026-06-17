859d416b01c8facd4e8a5a9ba9f342c661cbf273
James W <150986379+AutoTechWebDev@users.noreply.github.com>
2026-06-17 18:25:04 -0400

    Create enemies.js

---
+++ views/enemies.js
@@ -0,0 +1,15 @@
+export function renderEnemies(content){
+
+    content.innerHTML = `
+
+        <div class="panel">
+
+            <h2>Enemy Database</h2>
+
+            <p>
+                Coming in Module 2
+            </p>
+
+        </div>
+    `;
+}
