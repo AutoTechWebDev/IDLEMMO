8009b453d9c23bcd050a004c841f728f46c0c091
James W <150986379+AutoTechWebDev@users.noreply.github.com>
2026-06-17 18:25:17 -0400

    Create items.js

---
+++ views/items.js
@@ -0,0 +1,15 @@
+export function renderItems(content){
+
+    content.innerHTML = `
+
+        <div class="panel">
+
+            <h2>Item Database</h2>
+
+            <p>
+                Coming in Module 3
+            </p>
+
+        </div>
+    `;
+}
