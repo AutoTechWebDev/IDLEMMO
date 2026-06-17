7d1c16fedaeececf0ee6e3a1a8eff5d5d2a78765
James W <150986379+AutoTechWebDev@users.noreply.github.com>
2026-06-17 18:24:18 -0400

    Create storage.js

---
+++ js/storage.js
@@ -0,0 +1,17 @@
+export function save(key,data){
+
+    localStorage.setItem(
+        key,
+        JSON.stringify(data)
+    );
+}
+
+export function load(key){
+
+    const data =
+        localStorage.getItem(key);
+
+    return data
+        ? JSON.parse(data)
+        : null;
+}
